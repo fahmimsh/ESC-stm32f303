128bf9c7b04e81a55b22e157231c1ad5317e2335
Fahmi Mashuri <71054830+fahmimsh@users.noreply.github.com>
2022-10-13 11:23:27 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,9 @@
 // hey this is a comment by my repo
 console.log("Hello tHIS REPO already to open")
 console.log("Hello tHIS REPO already to open")
+
+function main(){
+  return null;
+}
+main()
+//console.log(main)
